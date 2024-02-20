be2b416bd406afb7dacbdf974d06a50b6f3ccab9
Sumaiya Begum <sumaiyabegum43@gmail.com>
2024-02-20 12:38:03 -0800

    slider added

---
+++ src/components/Slide/Slide.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import "./Slide.scss"
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import CatCard from '../CatCard/CatCard';
+
+
+
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 5
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
+
+function Slide({children}){
+
+
+    return (
+        <div className='slide'>
+            <div className="container">
+                <Carousel infinite={true} responsive={responsive}>
+                    {children}
+                </Carousel>;
+            </div>
+        </div>
+    )
+}
+
+export default Slide
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import TrustedBy from '../../components/trustedBy/TrustedBy'
+import Slide from '../../components/Slide/Slide'
+import { cards } from '../../data'
+import CatCard from '../../components/CatCard/CatCard'
 
 const Home = () => {
   return (
     <div className='home'>
       <Featured/>
+      <TrustedBy/>
+      <Slide>
+        {cards.map(card=>(
+          <CatCard key={card.id} item={card}/>
+        ))}
+      </Slide>
     </div>
   )
 }
